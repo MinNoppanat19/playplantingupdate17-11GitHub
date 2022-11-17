aea830c0df79a48ee978128c62aec4116bd6e825
MinNoppanat19 <89172431+MinNoppanat19@users.noreply.github.com>
2022-11-17 21:21:50 +0700

    min

---
+++ resources/views/layouts/inc/adminfooter.blade.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+    <div class="container-fluid">
+      <nav class="float-left">
+        <ul>
+          <li>
+            <a href="#">
+              Play Planting
+            </a>
+          </li>
+          <li>
+            <a href="#">
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+
+            </a>
+          </li>
+          <li>
+            <a href="#">
+
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright float-right">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>
+      </div>
+    </div>
+  </footer>
